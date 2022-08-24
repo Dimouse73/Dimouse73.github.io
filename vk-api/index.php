72381a66dad7d372dbdb24c62b9ac67d0d1ce16c
Dimouse73 <brazhenk0@icloud.com>
2022-08-24 21:52:33 +0400

    vk_api

---
+++ vk-api/index.php
@@ -0,0 +1,62 @@
+<?php
+
+    //https://dimouse73.github.io/vk-api/index.php
+
+
+    <?php
+
+    if (!isset($_REQUEST)) {
+    return;
+    }
+    
+    //Строка для подтверждения адреса сервера из настроек Callback API
+    $confirmation_token = '88106745';
+    
+    //Ключ доступа сообщества
+    $token = "71e536b0e8bb926c72af5eabb7aa10a076c3b33f42e1ddec433deb5285cca5827a9ed9d54876bb55bbda2"    
+    
+    //Получаем и декодируем уведомление
+    $data = json_decode(file_get_contents('php://input'));
+    
+    //Проверяем, что находится в поле "type"
+    switch ($data->type) {
+    //Если это уведомление для подтверждения адреса...
+    case 'confirmation':
+    //...отправляем строку для подтверждения
+    echo $confirmation_token;
+    break;
+    
+    //Если это уведомление о новом сообщении...
+    case 'message_new':
+    //...получаем id его автора
+    $user_id = $data->object->message->from_id;
+    //затем с помощью users.get получаем данные об авторе
+    $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.103"));
+    
+    //и извлекаем из ответа его имя
+    $user_name = $user_info->response[0]->first_name;
+    
+    //С помощью messages.send отправляем ответное сообщение
+    $request_params = array(
+    'message' => "Hello, {$user_name}!",
+    'peer_id' => $user_id,
+    'access_token' => $token,
+    'v' => '5.103',
+    'random_id' => '0'
+    );
+    
+    $get_params = http_build_query($request_params);
+    
+    file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
+    
+    //Возвращаем "ok" серверу Callback API
+    
+    echo('ok');
+    
+    break;
+    
+    }
+    ?> 
+
+
+?>    
